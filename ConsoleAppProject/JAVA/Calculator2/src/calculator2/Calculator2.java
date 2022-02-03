7a953f6099ddd0508f763ed2c80d8d61591c3106
Matthew Topping <100164552@student.burnley.ac.uk>
2022-02-03 14:45:10 +0000

    Started calc

---
+++ ConsoleAppProject/JAVA/Calculator2/src/calculator2/Calculator2.java
@@ -0,0 +1,38 @@
+
+public class Calculator2 {
+    String name;
+    
+    
+    
+    public int add(int x, int y){
+        return x + y;     
+    }
+            
+     public int subtract(int x, int y){
+        return x - y;     
+    }
+     
+    public int multiply(int x, int y){
+        return x - y;     
+    }
+    
+    
+     public int divide(int x, int y){
+        return x / y;     
+    }
+     
+    public int modulus(int x, int y){
+        return x % y;     
+    }
+            
+            
+            
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ConsoleAppProject/JAVA/Calculator2/src/calculator2/mainClass.java
@@ -0,0 +1,18 @@
+
+public class mainClass {
+    
+    public static void main(String[]) args){
+        Calculator2 c1 = new Calculator2();
+        Calculator2 c2 = new Calculator2();
+        
+        c1.name = "Name1";
+        c1.name = "Name1";
+        
+        
+        
+        
+        
+        
+    }
+    
+}
